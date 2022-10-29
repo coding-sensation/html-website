21bb1de26323b891b1bc82c1513175988230338f
talalzeini <talalelzeini@gmail.com>
2022-10-29 16:11:41 -0700

    removed all scripts

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById(
-  "head"
-).innerHTML += `<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' />
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css' />
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'></script>`;
